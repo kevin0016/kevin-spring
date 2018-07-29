3d830b7a91641e1067e039f2f8ffcb688f08d681
kevin0016 <1046522859@qq.com>
2018-07-29 17:59:37 +0800

    更改部分注释

--- kevin-template/src/com/itkevin/template/test/Run.java
+++ kevin-template/src/com/itkevin/template/test/Run.java
@@ -15,7 +15,7 @@ public class Run {
     }
 
     //SpringJDBC
-    //是java规范，各个数据库厂商自己去实现
+    //是java规范，各个数据库厂商自己去实现 j2EE规范就是个模板，根据这个模板来实现JAVAEE开发
     //1、加载驱动类DriverManager
     //2、建立连接
     //3、创建语句集(标准语句集、预处理语句集)(语句集？  MySQL、Oracle、SQLServer、Access)
